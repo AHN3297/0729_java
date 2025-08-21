25dc6f5e7c0a8612c4e96fcab257534cd311a217
AHN <ga33229977@gmail.com>
2025-08-21 11:45:51 +0900

    git_Practice

--- 14_Network/src/com/kh/network/run/TestClass.java
+++ 14_Network/src/com/kh/network/run/TestClass.java
@@ -14,7 +14,14 @@ public class TestClass {
 		// 진짜 마지막으로 한 번만해보자
 	}
 	public void hi() {
-		System.out.println();
+		System.out.println("드가자이");
+	}
+	public void printStatement() {
+		System.out.println("1. 워크스페이스에서 gitBash를 실행한다.");
+		System.out.println("2. git pull origin master");
+		System.out.println("3. git add .");
+		System.out.println("4. git commit -m 뭐했는지");
+		System.out.println("5. git push origin master");
 	}
 
 }
