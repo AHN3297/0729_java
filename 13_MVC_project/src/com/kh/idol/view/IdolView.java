6e0950fd11d0f156985616d12eb70e9421694977
AHN <ga33229977@gmail.com>
2025-08-21 17:13:58 +0900

    aespa post

--- 13_MVC_project/src/com/kh/idol/view/IdolView.java
+++ 13_MVC_project/src/com/kh/idol/view/IdolView.java
@@ -12,6 +12,7 @@ public class IdolView {
 	// 필드부
 	private Scanner sc = new Scanner(System.in);
 	private IdolController ic = new IdolController();
+	private Fan loginFan;
 	
 	// 생성자
 	
@@ -35,7 +36,7 @@ public class IdolView {
 			case 1: infoMenu(); break;
 			case 2: signUp(); break;
 			case 3: login(); break;
-			case 4: break;
+			case 4: boardMenu(); break;
 			case 5: break;
 			default : System.out.println("잘못된 메뉴를 선택하셨습니다.");
 			
@@ -164,10 +165,61 @@ public class IdolView {
 		String userId = sc.nextLine();
 		System.out.println("비밀번호를 입력해주세요 > ");
 		String userPwd = sc.nextLine();
-		ic.login(userId,userPwd);
+		
+		Fan fan = ic.login(userId, userPwd);
+		
+		if(fan != null) {
+			System.out.println(fan.getNickName()+ "님 환영합니다");
+			loginFan = fan;
+		}else {
+			System.out.println("로그인 실패!");
+		}
 		
 	}
 	
+	
+	private void boardMenu() {
+		System.out.println("\n 에스파 게시판입니다.");
+		System.out.println("1. 게시글 작성");
+		System.out.println("2. 게시글 전체 조회");
+		System.out.println("3. 게시르 상세 조회");
+		System.out.println("4. 메인메뉴로 돌아가기");
+		System.out.println("당신의 선택은? > ");
+		int menuNo =sc.nextInt();
+		
+		switch(menuNo) {
+		case 1: post(); break;
+		case 2: break;
+		case 3: break;
+		case 4: return;
+		}
+	}
+	
+	private void post() {
+		
+		System.out.println("\n게시글작성 서비스");
+		// 전제조건 : 로그인한 사용자만 게시글을 작성할 수 있음
+		// 1. 변수?
+		// 2. if?
+		// 3. for?
+		if(loginFan != null) {
+			
+			System.out.print("게시글 제목을 입력해주세요 > ");
+			String boardTitle = sc.nextLine();
+			
+			System.out.print("게시글 내용을 입력해주세요 > ");
+			String boardContent = sc.nextLine();
+			
+			
+			ic.post(boardTitle, boardContent, loginFan.getUserId());
+			
+			System.out.println("게시글작성 성공");
+			
+		} else {
+			System.out.println("로그인 후 이용가능한 서비스 입니다.");
+		}
+		
+	}
 	
 	
 	
